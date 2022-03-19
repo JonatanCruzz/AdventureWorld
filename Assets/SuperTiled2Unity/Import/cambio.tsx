194c57f4ab8c92ff5bd1176f7f5e6cb06fb06176
Jonatan <jonatanalexanderdiaz@gmail.com>
2022-03-19 09:15:56 -0400

    se subio

---
+++ Assets/SuperTiled2Unity/Import/cambio.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cambio" tilewidth="16" tileheight="16" tilecount="143" columns="11">
+ <image source="StaticTiles.png" width="176" height="208"/>
+</tileset>
