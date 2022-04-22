cbc91a43ca0061a3f7315a701d66cb04132e3b46
vitico hidalgo <viticohidalgo@hotmail.com>
2022-04-22 10:20:51 -0400

    cambios

--- Assets/ProCamera2D/Code/Helpers/Pixel Perfect/Editor/ProCamera2DPixelPerfectSpriteEditor.cs
+++ Assets/ProCamera2D/Code/Helpers/Pixel Perfect/Editor/ProCamera2DPixelPerfectSpriteEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 
 namespace Com.LuisPedroFonseca.ProCamera2D
@@ -97,3 +98,4 @@ namespace Com.LuisPedroFonseca.ProCamera2D
         }
     }
 }
+#endif
